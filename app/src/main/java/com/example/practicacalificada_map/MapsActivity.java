f2d65e2c3acff45e3dfff3f1b1edcd673edf472d
Edwarto <71796703+Edwarto@users.noreply.github.com>
2020-11-01 21:00:25 -0500

    Subiendo laboratorio

---
+++ app/src/main/java/com/example/practicacalificada_map/MapsActivity.java
@@ -0,0 +1,327 @@
+package com.example.practicacalificada_map;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.StreetViewPanoramaOptions;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final String TAG = "Estilo del mapa";
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private Spinner spn_location;
+    int iCurrentSelection = 0;
+    private double latitud = 35.680513;
+    private double longitud = 139.769051;
+
+    private Location myLocation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, mapFragment).commit();
+        mapFragment.getMapAsync(this);
+
+        spn_location = (Spinner) findViewById(R.id.spn_location);
+        String[] opciones = {"Normal", "Satélite", "Híbrido", "Terrarian"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, opciones);
+        spn_location.setAdapter(adapter);
+
+
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+
+
+        spn_location.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (iCurrentSelection != i) {
+                    switch (Integer.parseInt(String.valueOf(spn_location.getSelectedItemId()))) {
+                        case 0:
+                            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                            latitud = 35.680513;
+                            longitud = 139.769051;
+                            LatLng casa = new LatLng(latitud, longitud);
+                            mMap.addMarker(new MarkerOptions().position(casa).title("Marker in Japan"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(casa, 15));
+                            CalcularDistancia(latitud,longitud);
+
+                            break;
+                        case 1:
+                            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                            latitud = 52.516934;
+                            longitud = 13.403190;
+                            LatLng casa1 = new LatLng(latitud, longitud);
+                            mMap.addMarker(new MarkerOptions().position(casa1).title("Marker in Germany"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(casa1, 15));
+                            CalcularDistancia(latitud,longitud);
+
+                            break;
+                        case 2:
+                            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                            latitud = 41.902609;
+                            longitud = 12.494847;
+                            LatLng casa2 = new LatLng(latitud, longitud);
+                            mMap.addMarker(new MarkerOptions().position(casa2).title("Marker in Italy"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(casa2, 15));
+                            CalcularDistancia(latitud,longitud);
+                            break;
+                        case 3:
+                            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                            latitud = 48.843489;
+                            longitud = 2.355331;
+                            LatLng casa3 = new LatLng(latitud, longitud);
+                            mMap.addMarker(new MarkerOptions().position(casa3).title("Marker in French"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(casa3, 15));
+                            CalcularDistancia(latitud,longitud);
+                            break;
+                    }
+                } else {
+
+                }
+                iCurrentSelection = i;
+            }
+
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+    }
+
+    public void CalcularDistancia(Double latitud, Double longitud)
+    {
+        Location location_country = new Location("punto A");
+        location_country.setLatitude(latitud);
+        location_country.setLongitude(longitud);
+
+        float distance = location_country.distanceTo(myLocation);
+        double rptadistance = distance / 1000;
+        DecimalFormat myFormat = new DecimalFormat("0.000");
+        String myDoubleString = myFormat.format(rptadistance);
+
+        Toast.makeText(getApplicationContext(),"Distancia hasta este punto: " + myDoubleString + " km", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.map_options, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Change the map type based on the user's selection.
+        switch (item.getItemId()) {
+            case R.id.normal_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case R.id.hybrid_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case R.id.satellite_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case R.id.terrain_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        float zoom = 15;
+        //-18.054834, -70.249151
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsActivity.this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }else{
+            if(!mMap.isMyLocationEnabled())
+                mMap.setMyLocationEnabled(true);
+
+            LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            myLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (myLocation == null) {
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+                String provider = lm.getBestProvider(criteria, true);
+                myLocation = lm.getLastKnownLocation(provider);
+            }
+
+            if(myLocation!=null){
+                LatLng userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 14), 1500, null);
+            }
+        }
+
+
+        try {
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+
+
+        LatLng casa = new LatLng(latitud, longitud);
+        mMap.addMarker(new MarkerOptions().position(casa).title("Marker in Japan"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(casa, zoom));
+
+        GroundOverlayOptions casaOverlay = new GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.home))
+                .position(casa, 50);
+        mMap.addGroundOverlay(casaOverlay);
+
+        setMapLongClick(mMap);
+        setPoiClick(mMap);
+        enableMyLocation();
+        setInfoWindowClickToPanorama(mMap);
+
+        ////////
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0
+                        && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation();
+                    break;
+                }
+        }
+    }
+
+    private void setMapLongClick(final GoogleMap map) {
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                //map.addMarker(new MarkerOptions().position(latLng));
+                String snippet = String.format(Locale.getDefault(),
+                        "Lat: %1$.5f, Long: %2$.5f",
+                        latLng.latitude,
+                        latLng.longitude);
+                map.addMarker(new MarkerOptions()
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                        .position(latLng)
+                        .title(getString(R.string.app_name))
+                        .snippet(snippet));
+            }
+        });
+    }
+
+    private void setPoiClick(final GoogleMap map) {
+        map.setOnPoiClickListener(new GoogleMap.OnPoiClickListener() {
+            @Override
+            public void onPoiClick(PointOfInterest poi) {
+                Marker poiMarker = mMap.addMarker(new MarkerOptions()
+                        .position(poi.latLng)
+                        .title(poi.name));
+                poiMarker.setTag("poi");
+                poiMarker.showInfoWindow();
+            }
+
+        });
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+
+    private void setInfoWindowClickToPanorama(GoogleMap map) {
+        map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                if (marker.getTag() == "poi") {
+                    StreetViewPanoramaOptions options =
+                            new StreetViewPanoramaOptions().position(
+                                    marker.getPosition());
+                    SupportStreetViewPanoramaFragment streetViewFragment
+                            = SupportStreetViewPanoramaFragment
+                            .newInstance(options);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container,
+                                    streetViewFragment)
+                            .addToBackStack(null).commit();
+                }
+            }
+        });
+
+    }
+}
